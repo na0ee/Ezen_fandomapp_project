a2e53ab59b845b4e87117e19f0e3486d047105b2
gaeun <xybeddd6@gmail.com>
2026-07-14 15:16:35 +0900

    Layer 커뮤니티 페이지 수정

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { HomePage } from "./pages/HomePage";
 import { MagazinePage } from "./pages/MagazinePage";
 import { QuestionPage } from "./pages/QuestionPage";
 import { ResultPage } from "./pages/ResultPage";
+import { CommunityPage } from "./pages/CommunityPage";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Route path="/magazine" element={<MagazinePage />} />
       <Route path="/question" element={<QuestionPage />} />
       <Route path="/result" element={<ResultPage />} />
+      <Route path="/community" element={<CommunityPage />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
