0d69c351e64b85b6015b52fa2cec3abedf331283
joyeeun23-commit <joyeeun23@gmail.com>
2026-07-15 16:29:53 +0900

    feature/yeeun_magazine

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { CommunityPage } from "./pages/CommunityPage";
 import { EventPage } from "./pages/EventPage";
 import { HomePage } from "./pages/HomePage";
-import MagazinePage from "./pages/Magazine";
+import MagazinePage from "./pages/Magazine/Magazine";
+import Magazine1 from "./pages/Magazine/Magazine_1";
+import Magazine2 from "./pages/Magazine/Magazine_2";
+import Magazine3 from "./pages/Magazine/Magazine_3";
 import { QuestionPage } from "./pages/QuestionPage";
 import { ResultPage } from "./pages/ResultPage";
-import MyPage from "./pages/MyPage";
+import MyPage from "./pages/Mypage/MyPage";
 import SearchResults from "./pages/SearchResults";
 import Category from "./pages/Category";
 import Search from "./pages/Search";
@@ -17,6 +20,9 @@ export default function App() {
       <Route path="/event" element={<EventPage />} />
       <Route path="/community" element={<CommunityPage />} />
       <Route path="/magazine" element={<MagazinePage />} />
+      <Route path="/magazine/niche-trend" element={<Magazine1 />} />
+      <Route path="/magazine/perfume-longevity" element={<Magazine2 />} />
+      <Route path="/magazine/seasonal-guide" element={<Magazine3 />} />
       <Route path="/question" element={<QuestionPage />} />
       <Route path="/result" element={<ResultPage />} />
       <Route path="/mypage" element={<MyPage />} />
